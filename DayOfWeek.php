2475462be1178d609b71e731ed8b9a766bd514bb
jgrondin <jgrondin@touchandplay.fr>
2015-06-19 14:43:15 +0200

    First commit of the class

---
+++ DayOfWeek.php
@@ -0,0 +1,28 @@
+<?php
+namespace DbTools;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+/**
+ * DayOfWeekFunction ::= "DAYOFWEEK" "(" ArithmeticPrimary ")"
+ */
+class DayOfWeek extends FunctionNode {
+	
+    public $dateExpression = null;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER); 
+        $parser->match(Lexer::T_OPEN_PARENTHESIS); 
+        $this->dateExpression = $parser->ArithmeticPrimary();  
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS); 
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'DAYOFWEEK(' .
+            $this->dateExpression->dispatch($sqlWalker) . 
+        ')'; // (7)
+    }
+}
